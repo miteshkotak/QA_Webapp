223a0570c144efa3d521643a3c113484fb87bdc5
Mitesh Kotak <miteshkotak7@gmail.com>
2020-08-26 17:39:59 +0200

    intial upload pf repo files
    intial upload pf repo files

---
+++ precache-manifest.0633684405e528912daca6d982c86891.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa6f0e70df05c7e0dfb8c906017352b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "faae42391aa88ae38954",
+    "url": "./static/css/2.0981ad52.chunk.css"
+  },
+  {
+    "revision": "45b2bbd044655b5d0fdc",
+    "url": "./static/css/main.39adee04.chunk.css"
+  },
+  {
+    "revision": "faae42391aa88ae38954",
+    "url": "./static/js/2.416f44b4.chunk.js"
+  },
+  {
+    "revision": "45b2bbd044655b5d0fdc",
+    "url": "./static/js/main.666e1e55.chunk.js"
+  },
+  {
+    "revision": "aa81c90ce6daa6eb4862",
+    "url": "./static/js/runtime-main.ca18c5c4.js"
+  },
+  {
+    "revision": "3c7f0a2cd285e32b1aeb04aada4c0fdf",
+    "url": "./static/media/Rois-Bold.3c7f0a2c.woff"
+  },
+  {
+    "revision": "92c214e99f810f2e47b64d43955eeb01",
+    "url": "./static/media/Rois-Bold.92c214e9.woff2"
+  },
+  {
+    "revision": "c1f49326c93167c8da8f1dad7d0aea62",
+    "url": "./static/media/Rois-Medium.c1f49326.woff2"
+  },
+  {
+    "revision": "f930acd16f887c4067f0fc5f13c3e39a",
+    "url": "./static/media/Rois-Medium.f930acd1.woff"
+  }
+]);
